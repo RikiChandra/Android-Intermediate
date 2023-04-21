5204aaf8cfdec5901fa688610503dce7d2271158
RikiChandra <49185211+RikiChandra@users.noreply.github.com>
2023-04-21 15:58:55 +0700

    fix widget

--- app/src/main/java/com/example/sharingapp/widget/WidgetRemoteViewsFactory.kt
+++ app/src/main/java/com/example/sharingapp/widget/WidgetRemoteViewsFactory.kt
@@ -1,8 +1,11 @@
 package com.example.sharingapp.widget
 
 
+import android.appwidget.AppWidgetManager
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import android.widget.RemoteViews
 import android.widget.RemoteViewsService
 import androidx.datastore.core.DataStore
@@ -13,7 +16,9 @@ import com.example.sharingapp.R
 import com.example.sharingapp.api.ApiConfig
 import com.example.sharingapp.responses.Story
 import com.example.sharingapp.setting.SharedPreference
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 
@@ -29,25 +34,29 @@ class WidgetRemoteViewsFactory(private val context: Context, private val intent:
     }
 
     override fun onDataSetChanged() {
-        // mengambil data dari server menggunakan retrofit
+        // Mengambil data dari server menggunakan Retrofit
         val apiService = ApiConfig.getApiService()
-        val sharedPreferences = SharedPreference.getInstance(
-            context.applicationContext.dataStore
-        )
+        val sharedPreferences = SharedPreference.getInstance(context.applicationContext.dataStore)
 
-        val token = runBlocking {
-            sharedPreferences.ambilToken().first()
+        GlobalScope.launch {
+            val token = sharedPreferences.ambilToken().first()
+            if (token.isNotEmpty()) {
+                try {
+                    val storyResponses = apiService.getStories("Bearer $token", 1, 10, null)
+                    items = storyResponses.listStory ?: emptyList()
+                    // Update data pada widget
+                    val appWidgetManager = AppWidgetManager.getInstance(context)
+                    val appWidgetIds = appWidgetManager.getAppWidgetIds(ComponentName(context, WidgetStory::class.java))
+                    appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.stack_view)
+                } catch (e: Exception) {
+                    // Tangani kesalahan di sini jika diperlukan
+                    Log.e("WidgetRemoteViewsFactory", "onDataSetChanged: ${e.message}")
+                }
+            }
         }
-
-//        if (token.isNotEmpty()) {
-//            val call = apiService.getStories("Bearer $token", 1, 10, null)
-//            val response = call.execute()
-//
-//            if (response.isSuccessful) {
-//                items = (response.body()?.listStory ?: emptyList()) as List<Story>
-//            }
-//        }
     }
+
+
 
     override fun onDestroy() {}
 
