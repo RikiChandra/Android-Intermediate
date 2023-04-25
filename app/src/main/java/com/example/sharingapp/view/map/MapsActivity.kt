e0c95bb6e50f3b0c7111d255d77f958610a66541
RikiChandra <49185211+RikiChandra@users.noreply.github.com>
2023-04-25 11:36:21 +0700

    clean code

--- app/src/main/java/com/example/sharingapp/view/map/MapsActivity.kt
+++ app/src/main/java/com/example/sharingapp/view/map/MapsActivity.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.datastore.core.DataStore
@@ -15,28 +14,19 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.sharingapp.R
 import com.example.sharingapp.api.ApiConfig
 import com.example.sharingapp.databinding.ActivityMapsBinding
-import com.example.sharingapp.responses.StoryResponses
 import com.example.sharingapp.setting.SharedPreference
 import com.example.sharingapp.setting.ViewModelFactory
-import com.example.sharingapp.view.main.MainViewModel
-
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.runBlocking
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
-    private val apiService = ApiConfig.getApiService()
     private lateinit var viewModel: MapsViewModel
     private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
